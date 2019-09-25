6a1fbb346ec72feeceb872ba762f15aed05e31ef
Quentin Guimard <quentin.guimard@mythalesgroup.io>
2019-09-25 08:57:33 +0200

    Generator test added

--- src/test/java/javaben/GeneratorTest.java
+++ src/test/java/javaben/GeneratorTest.java
@@ -1,5 +1,34 @@
 package javaben;
 
-public class GeneratorTest {
+import org.junit.Test;
 
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class GeneratorTest {
+    private final static int SIZE = 10000;
+    private final Generator generator = new Generator(516310114);
+
+    @Test
+    public void unsortedListGeneratorTest() {
+        List<Integer> list = generator.unsortedListGenerator(SIZE);
+        assertEquals(SIZE, list.size());
+    }
+
+    @Test
+    public void sortedListGeneratorTest() {
+        List<Integer> list = generator.sortedListGenerator(SIZE, "asc");
+        assertEquals(SIZE, list.size());
+        for (int i = 1; i < SIZE; i++) {
+            assertTrue(list.get(i) > list.get(i - 1));
+        }
+
+        list = generator.sortedListGenerator(SIZE, "desc");
+        assertEquals(SIZE, list.size());
+        for (int i = 1; i < SIZE; i++) {
+            assertTrue(list.get(i) < list.get(i - 1));
+        }
+    }
 }
