7256a3da276c5b2c336c0f527156d997e39382e4
Florian Salord <florian.salord@etu.univ-cotedazur.fr>
2019-09-30 11:21:32 +0200

    updated main

--- src/main/java/javaben/Main.java
+++ src/main/java/javaben/Main.java
@@ -10,63 +10,59 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
-        long seed = 516310114;
+	public static void main(String[] args) {
+		long seed = 516310114;
 
 
+		long iterations = (long) Math.pow(10, 6);
 
-        long n = (long) Math.pow(10, 6);
-        for (int i = 0; i < 10; i++) {
-            int size = (int) Math.pow(2, i);
-            System.out.print("size : " + size + " ");
-            launch(seed, size, n);
-        }
+		int maxPow = 1;
+
+		for (int i = 0; i < maxPow; i++) {
+			int size = (int) Math.pow(2, i);
+			System.out.println("size : " + size + " ");
+			launch(seed, size, iterations);
+		}
 
 
-    }
+	}
 
-    private static void launch(long seed, int size, long n) {
-        List<Callable> list = new ArrayList<>();
-        //list.add(new Addition());
-        //list.add(new Multiplication());
+	private static void launch(long seed, int size, long iterations) {
+		List<Callable> list = new ArrayList<>();
+		list.add(new Addition());
+		list.add(new Multiplication());
 
-        for (Callable item : list) {
-            long total = 0;
-            total = total + Benchmark.bench(item, n, size, seed);
-            total = total - Benchmark.bench(new Empty(), n, size, seed);
-            System.out.format("%-20s%-30s%s%n", item.getClass().getSimpleName() + " :", "total (ns) : " + total, "per op (ns) : " + total / (double) n);
-        }
+		for (Callable item : list) {
+			long total = 0;
+			total = total + Benchmark.bench(item, iterations, size, seed);
+			total = total - Benchmark.bench(new Empty(), iterations, size, seed);
+			System.out.format("%-20s%-30s%s%n", item.getClass().getSimpleName() + " :", "total (ns) : " + total, "per op (ns) : " + total / (double) iterations);
+		}
 
-        n = (long) Math.pow(10, 6);
+		iterations = (long) Math.pow(10, 6);
 
-        list.clear();
-        list.add(new CountingSort());
-//        list.add(new FusionSort());
-//        list.add(new HeapSort());
-//        list.add(new InsertionSort());
-        list.add(new NativeSort());
-        list.add(new QuickSort());
-        //list.add(new CountingSort());
-        list.add(new MergeSort());
-        //list.add(new HeapSort());
-        //list.add(new InsertionSort());
-        //list.add(new NativeSort());
-        //list.add(new QuickSort());
-        //list.add(new SelectionSort());
-        //list.add(new SmoothSort());
+		list.clear();
+		list.add(new CountingSort());
+		list.add(new HeapSort());
+		list.add(new InsertionSort());
+		list.add(new QuickSort());
+		list.add(new MergeSort());
+		list.add(new NativeSort());
+		list.add(new SelectionSort());
+		list.add(new SmoothSort());
 
-        for (Callable item : list) {
-            long total = 0;
-            total = total + Benchmark.bench(item, n, size, seed);
-            total = total - Benchmark.bench(new EmptySort(), n, size, seed);
-            System.out.format("%-20s%-30s%s%n", item.getClass().getSimpleName() + " :", "total (ns) : " + total, "per op (ns) : " + total / (double) n);
+		for (Callable item : list) {
+			long total = 0;
+			total = total + Benchmark.bench(item, iterations, size, seed);
+			total = total - Benchmark.bench(new EmptySort(), iterations, size, seed);
+			System.out.format("%-20s%-30s%s%n", item.getClass().getSimpleName() + " :", "total (ns) : " + total, "per op (ns) : " + total / (double) iterations);
 
-        }
-    }
+		}
+	}
 
-    private double factor(int size, long time) {
-        double sizeLog = Math.log(size);
-        double timeLog = Math.log(time);
-        return 0;
-    }
+	private double factor(int size, long time) {
+		double sizeLog = Math.log(size);
+		double timeLog = Math.log(time);
+		return 0;
+	}
 }
