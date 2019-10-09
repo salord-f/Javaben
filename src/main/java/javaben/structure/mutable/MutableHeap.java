b98d8f85bc87648e05e226dbd0a4c108c2dd7060
Quentin Guimard <quentin.guimard@mythalesgroup.io>
2019-10-09 10:21:46 +0200

    Mutable heap access changed

--- src/main/java/javaben/structure/mutable/MutableHeap.java
+++ src/main/java/javaben/structure/mutable/MutableHeap.java
@@ -1,7 +1,7 @@
 package javaben.structure.mutable;
 
 public class MutableHeap extends BinaryTree {
-    void add(int e) {
+    public void add(int e) {
         elements.add(e);
         int elementIndex = elements.size() - 1;
         while (!isRoot(elementIndex) && !isCorrectChild(elementIndex)) {
@@ -11,34 +11,7 @@ public class MutableHeap extends BinaryTree {
         }
     }
 
-    boolean isRoot(int index) {
-        return index == 0;
-    }
-
-    boolean isCorrectChild(int index) {
-        return isCorrect(parentIndex(index), index);
-    }
-
-    boolean isCorrect(int parentIndex, int childIndex) {
-        if (!isValidIndex(parentIndex) || !isValidIndex(childIndex)) {
-            return true;
-        }
-
-        return elementAt(parentIndex) - elementAt(childIndex) < 0;
-    }
-
-    boolean isValidIndex(int index) {
-        return index < elements.size();
-    }
-
-    void swap(int index1, int index2) {
-        int element1 = elementAt(index1);
-        int element2 = elementAt(index2);
-        elements.set(index1, element2);
-        elements.set(index2, element1);
-    }
-
-    int pop() {
+    public int pop() {
         if (isEmpty()) {
             throw new IllegalStateException("You cannot pop from an empty heap");
         }
@@ -59,15 +32,42 @@ public class MutableHeap extends BinaryTree {
         return result;
     }
 
-    boolean isLeaf(int index) {
+    private boolean isRoot(int index) {
+        return index == 0;
+    }
+
+    private boolean isCorrectChild(int index) {
+        return isCorrect(parentIndex(index), index);
+    }
+
+    private boolean isCorrect(int parentIndex, int childIndex) {
+        if (!isValidIndex(parentIndex) || !isValidIndex(childIndex)) {
+            return true;
+        }
+
+        return elementAt(parentIndex) - elementAt(childIndex) < 0;
+    }
+
+    private boolean isValidIndex(int index) {
+        return index < elements.size();
+    }
+
+    private void swap(int index1, int index2) {
+        int element1 = elementAt(index1);
+        int element2 = elementAt(index2);
+        elements.set(index1, element2);
+        elements.set(index2, element1);
+    }
+
+    private boolean isLeaf(int index) {
         return !isValidIndex(leftChildIndex(index));
     }
 
-    boolean isCorrectParent(int index) {
+    private boolean isCorrectParent(int index) {
         return isCorrect(index, leftChildIndex(index)) && isCorrect(index, rightChildIndex(index));
     }
 
-    int smallerChildIndex(int index) {
+    private int smallerChildIndex(int index) {
         int leftChildIndex = leftChildIndex(index);
         int rightChildIndex = rightChildIndex(index);
 
