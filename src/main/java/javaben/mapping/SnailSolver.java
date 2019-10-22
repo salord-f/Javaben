7c2442e4e15d02e9d106a2c85f6c304799d0a970
Florian Salord <florian.salord@etu.univ-cotedazur.fr>
2019-10-22 21:46:02 +0200

    Fix snail

--- src/main/java/javaben/mapping/SnailSolver.java
+++ src/main/java/javaben/mapping/SnailSolver.java
@@ -5,11 +5,11 @@ public class SnailSolver extends Solver {
 	@Override
 	public String solve(Network network) {
 		int x = 0, y = 0, dx = 0, dy = -1;
-		int snailSize = (int) Math.ceil(Math.sqrt(network.getVerticesCount()));
+		int snailSize = (int) Math.ceil(Math.sqrt(network.getVerticesCount())) + 1;
 
 		for (int i = 0; i < network.getVerticesCount(); ) {
 			if (-snailSize / 2 < x && x <= snailSize / 2 && -snailSize < y && y <= snailSize) {
-				super.build(x, y);
+				super.build(x + snailSize, y + snailSize);
 				i++;
 			}
 			if (x == y || (x < 0 && x == -y) || (x > 0 && x == 1 - y)) {
@@ -20,7 +20,7 @@ public class SnailSolver extends Solver {
 			x += dx;
 			y += dy;
 		}
-
+		
 		return result.toString();
 	}
 
