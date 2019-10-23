2b26f74f786beb14451e7a592f37413743ff4b41
Florian Salord <florian.salord@etu.univ-cotedazur.fr>
2019-10-23 08:30:57 +0200

    Added weighted vertex list

--- src/main/java/javaben/mapping/EdgeSetNetwork.java
+++ src/main/java/javaben/mapping/EdgeSetNetwork.java
@@ -1,27 +1,42 @@
 package javaben.mapping;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class EdgeSetNetwork extends Network {
-    private Set<Edge> edges;
+	private Set<Edge> edges;
 
-    public EdgeSetNetwork() {
-        edges = new HashSet<>();
-    }
+	public EdgeSetNetwork() {
+		edges = new HashSet<>();
+	}
 
-    @Override
-    public void parseNetwork(String network) {
-        super.parseNetwork(network);
-        String[] edges = network.substring(network.indexOf("\n") + 1).split("\n");
-        for (String edge : edges) {
-            String[] v = edge.split(" ");
-            this.edges.add(new Edge(new Vertex(Integer.parseInt(v[0])), new Vertex(Integer.parseInt(v[1]))));
-        }
+	@Override
+	public void parseNetwork(String network) {
+		super.parseNetwork(network);
+		String[] edges = network.substring(network.indexOf("\n") + 1).split("\n");
+		for (String edge : edges) {
+			String[] v = edge.split(" ");
+			this.edges.add(new Edge(new Vertex(Integer.parseInt(v[0])), new Vertex(Integer.parseInt(v[1]))));
+		}
 
-    }
+	}
 
-    public Set<Edge> getEdges() {
-        return edges;
-    }
+	public Set<Edge> getEdges() {
+		return edges;
+	}
+
+	public List<Integer> getWeightedVertex() {
+		Map<Integer, Integer> vertexCount = new LinkedHashMap<>();
+
+		for (Edge edge : edges) {
+			vertexCount.merge(edge.getVertices()[0].getId(), 1, Integer::sum);
+			vertexCount.merge(edge.getVertices()[1].getId(), 1, Integer::sum);
+		}
+
+		List<Integer> ordered = new ArrayList<>();
+		vertexCount.entrySet().stream()
+				.sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+				.forEach(v -> ordered.add(v.getKey()));
+
+		return ordered;
+	}
 }
--- src/test/java/javaben/mapping/EdgeSetNetworkTest.java
+++ src/test/java/javaben/mapping/EdgeSetNetworkTest.java
@@ -2,6 +2,9 @@ package javaben.mapping;
 
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 
 public class EdgeSetNetworkTest {
@@ -13,5 +16,17 @@ public class EdgeSetNetworkTest {
         assertEquals(24, edgeSetNetwork.getEdges().size());
         assertEquals(16, edgeSetNetwork.getVerticesCount());
         assertEquals(24, edgeSetNetwork.getEdgesCount());
+    }
+
+    @Test
+    public void weightedVertexTest() {
+        String input = "5 5\n0 4\n1 0\n2 0\n0 2\n2 4\n";
+        EdgeSetNetwork network = new EdgeSetNetwork();
+        network.parseNetwork(input);
+        List<Integer> array = network.getWeightedVertex();
+        assertEquals(0, (int) array.get(0));
+        assertEquals(2, (int) array.get(1));
+        assertEquals(4, (int) array.get(2));
+        assertEquals(1, (int) array.get(3));
     }
 }
